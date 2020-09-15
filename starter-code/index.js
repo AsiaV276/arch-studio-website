0458971653e6a7530291a1f1a1ba66a3a38f60b7
Asia Valentine <asiavalentine22@gmail.com>
2020-09-15 00:30:51 -0400

    menu open and close

---
+++ starter-code/index.js
@@ -0,0 +1,8 @@
+const openMenu = () => {
+    const menuContent = document.getElementById('menu-content');
+    if (menuContent.style.display == 'block') {
+      menuContent.style.display = 'none';
+    } else {
+      menuContent.style.display = 'block';
+    }
+} 
